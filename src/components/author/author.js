b4776a4a3a6097a670afb06ccdf0e4fbd8141798
TheMiksa <michael.gritskov@gmail.com>
2020-11-08 23:38:00 +0200

    a bit refactoring

--- src/components/author/author.js
+++ src/components/author/author.js
@@ -3,6 +3,18 @@ import {Button, Text, View} from "react-native";
 import SearchPanel from "../search-panel";
 
 const Author = ({author, posts, handleAuthor, isAuthorSelected}) => {
+
+    return (
+                <View style={{paddingBottom: 15}}>
+                    {isAuthorSelected ? (<AuthorFullPage author={author} posts={posts} handleAuthor={handleAuthor}/>) : (
+                                         <AuthorShortPage author={author} posts={posts} handleAuthor={handleAuthor}/>)}
+                </View>
+            )
+
+};
+
+
+const AuthorFullPage = ({author, posts, handleAuthor}) => {
     const [search, setSearch] = useState("");
 
     const onPostsSearch = (post) => {
@@ -11,46 +23,37 @@ const Author = ({author, posts, handleAuthor, isAuthorSelected}) => {
             post.body.toLowerCase().includes(search.toLowerCase()));
     };
 
-
     return (
-        isAuthorSelected ? (
-                <View style={{paddingBottom: 15}}>
-                    <Text>{`${author.name}'s Posts`}</Text>
-                    <SearchPanel onSearch={setSearch}/>
-                    {isAuthorSelected && posts.filter(onPostsSearch).map(({title, body, id}) => (
-                        <View key={id} style={{paddingBottom: 10}}>
-                            <Text style={{paddingTop: 5, paddingBottom: 5}}>Title: {title}</Text>
-                            <Text>Body: {body}</Text>
-                        </View>
-                    ))}
-                    <Button onPress={() => {handleAuthor(author.id)}}
-                            style={{backgroundColor: 'aqua', margin: "auto"}}
-                            title="Back to Search"
-                    />
-                </View>
-            ): ( <View style={{paddingBottom: 15}}>
-                {isAuthorSelected && <SearchPanel onSearch={setSearch}/>}
-                <Text>{author.name}</Text>
-                <Text style={{fontSize: 10, color: 'grey'}}>{author.email}</Text>
-                {isAuthorSelected && posts.filter(onPostsSearch).map(({title, body, id}) => (
+        <React.Fragment>
+            <Text>{`${author.name}'s`}</Text>
+            <SearchPanel onSearch={setSearch}/>
+            {posts.filter(onPostsSearch)
+                .map(({title, body, id}) => (
                     <View key={id} style={{paddingBottom: 10}}>
                         <Text style={{paddingTop: 5, paddingBottom: 5}}>Title: {title}</Text>
                         <Text>Body: {body}</Text>
                     </View>
                 ))}
-                <Button onPress={() => {handleAuthor(author.id)}}
-                        style={{backgroundColor: 'aqua', margin: "auto"}}
-                        title={`${posts.length} posts`}
-                />
-            </View>
-        ));
-
+            <Button onPress={() => {handleAuthor(author.id)}}
+                    style={{backgroundColor: 'aqua', margin: "auto"}}
+                    title="Back to Search"
+            />
+        </React.Fragment>
+    );
 };
 
-const AuthorContent = ({}) => {
-
+const AuthorShortPage = ({author, posts, handleAuthor}) => {
     return (
-        null
+        <React.Fragment>
+            <Text>{author.name}</Text>
+            <Text style={{fontSize: 10, color: 'grey'}}>
+                {author.email}
+            </Text>
+            <Button onPress={() => {handleAuthor(author.id)}}
+                    style={{backgroundColor: 'aqua', margin: "auto"}}
+                    title={`${posts.length} posts`}
+            />
+        </React.Fragment>
     );
 };
 
